d24fbc7f0e8c58d25aeb01a3b7d8976e1e02bc09
conor <conor.houghton@bristol.ac.uk>
2022-09-29 21:26:44 +0100

    m

--- linear.jl
+++ linear.jl
@@ -1,7 +1,7 @@
 using Random, Distributions, Turing
 
-beta1= 4.0
-beta2= -2.0
+beta1= 0.4
+beta2= -0.2
 beta3= 1.0
 
 function make_data(beta,x,sigma)
@@ -13,36 +13,34 @@ beta=[beta1,beta2,beta3]
 
 dataSigma=10.0::Float64
 
-n_data=100
+nData=100
 
-x=rand(Normal(0.0, dataSigma), 100,3)
+x=rand(Normal(0.0, dataSigma), nData,3)
 
 sigmaNoise=0.25
 
+
 y=make_data(beta,x,sigmaNoise)
 
-println(size(x))
-println(size(y))
 
-priorSigma = 2.0
-
-@model function regression(x, y, priorSigma)
+@model function regression(x, y)
 
     intercept ~ Normal(0, sqrt(3))
     
     sigma2 ~ truncated(Normal(0, 100), 0, Inf)
-    
-    beta1 ~ Normal(0, priorSigma)
-    beta2 ~ Normal(0, priorSigma)
-    beta3 ~ Normal(0, priorSigma)
 
-    mu = intercept.+x*[beta1,beta2,beta3]
-        
+    priorSigma ~ Exponential(1)
+    beta ~ MvNormal([0.0,0.0,0.0], priorSigma)
+
+    mu = intercept.+x*beta
+
+    
+    
     y ~ MvNormal(mu,sqrt(sigma2))
     
-end;
+end
 
-model = regression(x, y,priorSigma)
-chain = sample(model, HMC(0.05, 10), MCMCThreads(), 1_500, 3)
+model = regression(x, y)
+chain = sample(model, NUTS(), MCMCThreads(), 1_500, 3)
 
 describe(chain)
